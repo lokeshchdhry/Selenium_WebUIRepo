47b1a2da4eccb39be1448cbb73f5ae3a2982a2b1
Lokesh Choudhary <lchoudhary@axway.com>
2019-08-18 15:54:16 -0700

    Added comment Appu

--- DemoStaticDrodown.java
+++ DemoStaticDrodown.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.ui.Select;
 public class DemoStaticDrodown {
 
 	public static void main(String[] args) throws InterruptedException {
-		
+		//adding comment
 		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.orbitz.com/");
